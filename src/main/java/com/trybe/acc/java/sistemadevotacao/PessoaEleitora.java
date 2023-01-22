6fcdd74c8430c6cc8bd178cb4e4a621c9a3d009b
WBomfim <willianbomfim@hotmail.com>
2023-01-22 11:17:21 -0300

    feat: Criado a classe PessoaEleitora

--- src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,23 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class PessoaEleitora {
+/**
+ * Class PessoaEleitora.
+ *
+ */
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
+  
+  public PessoaEleitora(String nome, String cpf) {
+    super(nome);
+    this.cpf = cpf;
+  }
 
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+  
 }
