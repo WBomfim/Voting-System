64f58cd5f9031977f7840abcdbc5d6339673e0ef
WBomfim <willianbomfim@hotmail.com>
2023-01-22 17:57:18 -0300

    feat: Criado os métodos para exibição dos menus na classe Principal

--- src/main/java/com/trybe/acc/java/sistemadevotacao/Principal.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/Principal.java
@@ -1,14 +1,58 @@
 package com.trybe.acc.java.sistemadevotacao;
 
+import java.util.Scanner;
+
 /**
  * Class Principal.
  *
  */
 public class Principal {
+  private static GerenciamentoVotacao gerenciamentoVotacao;
+  private static Scanner scan;
+  private static short opcao;
 
+  /**
+   * Método Principal.
+   * 
+   */
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+    gerenciamentoVotacao = new GerenciamentoVotacao();
+    scan = new Scanner(System.in);
+    
+    do {
+      
+    } while (opcao != 2);
+    
+    do {
+      
+    } while (opcao != 2);
+    
+    do {
+      
+    } while (opcao != 3);
+  }
+  
+  private static void menuCadastroCandidato() {
+    System.out.println("Cadastrar pessoa candidata?");
+    System.out.println("1 - Sim");
+    System.out.println("2 - Não");
+    System.out.println("Entre com o número correspondente à opção desejada:");
+    opcao = scan.nextShort();
+  }
+  
+  private static void menuCadastroEleitor() {
+    System.out.println("Cadastrar pessoa eleitora?");
+    System.out.println("1 - Sim");
+    System.out.println("2 - Não");
+    System.out.println("Entre com o número correspondente à opção desejada:");
+    opcao = scan.nextShort();
   }
 
+  private static void menuVotacao() {
+    System.out.println("Entre com o número correspondente à opção desejada:");
+    System.out.println("1 - Votar");
+    System.out.println("2 - Resultado Parcial");
+    System.out.println("3 - Finalizar Votação");
+    opcao = scan.nextShort();
+  }
 }
