41f1d58390f2d5fffe227597e7f5e3310dc38407
WBomfim <willianbomfim@hotmail.com>
2023-01-22 11:13:28 -0300

    feat: Criado a classe PessoaCandidata

--- src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,40 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class PessoaCandidata {
+/**
+ * Class PessoaCandidata.
+ *
+ */
+public class PessoaCandidata extends Pessoa {
+  private Integer numero;
+  private Integer votos;
+  
+  /**
+   * Construtor.
+   *
+   */
+  public PessoaCandidata(String nome, Integer numero) {
+    super(nome);
+    this.numero = numero;
+    this.votos = 0;
+  }
 
+  public Integer getNumero() {
+    return numero;
+  }
+
+  public void setNumero(Integer numero) {
+    this.numero = numero;
+  }
+
+  public Integer getVotos() {
+    return votos;
+  }
+
+  public void setVotos(Integer votos) {
+    this.votos = votos;
+  }
+  
+  public void receberVoto() {
+    this.votos += 1;
+  }
 }
