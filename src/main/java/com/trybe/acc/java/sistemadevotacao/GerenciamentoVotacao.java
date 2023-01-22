e0ec18acf2dca2eb5773c28ad8f64767cef31745
WBomfim <willianbomfim@hotmail.com>
2023-01-22 10:57:36 -0300

    feat: Criado a classe abstrata Pessoa.

--- src/main/java/com/trybe/acc/java/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,5 +1,19 @@
 package com.trybe.acc.java.sistemadevotacao;
+import java.util.ArrayList;
 
+
+/**
+ * Class GerenciamentoVotacao.
+ *
+ */
 public class GerenciamentoVotacao {
-
+  private ArrayList<E> pessoasCandidatas = new ArrayList<>();
+  private ArrayList<E> pessoasEleitoras = new ArrayList<>();
+  private ArrayList<String> cpfComputado = new ArrayList<String>();
+  private Integer totalVotos = 0;
+  
+  public void cadastrarPessoaCandidata(String nome, Integer numero) {
+    
+  }
+  
 }
