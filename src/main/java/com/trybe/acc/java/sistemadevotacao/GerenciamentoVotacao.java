6154dec3897280514f30f3e76066489d257c0de7
WBomfim <willianbomfim@hotmail.com>
2023-01-22 11:48:56 -0300

    feat: Criado a classe GerenciamentoVotacao Criado também os métodos cadastrarPessoaCandidata e cadastrarPessoaEleitora

--- src/main/java/com/trybe/acc/java/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/GerenciamentoVotacao.java
@@ -7,13 +7,51 @@ import java.util.ArrayList;
  *
  */
 public class GerenciamentoVotacao {
-  private ArrayList<E> pessoasCandidatas = new ArrayList<>();
-  private ArrayList<E> pessoasEleitoras = new ArrayList<>();
-  private ArrayList<String> cpfComputado = new ArrayList<String>();
-  private Integer totalVotos = 0;
+  private ArrayList<PessoaCandidata> pessoasCandidatas;
+  private ArrayList<PessoaEleitora> pessoasEleitoras;
+  private ArrayList<String> cpfComputado;
+  private Integer totalVotos;
   
+  /**
+   * Contrutor.
+   */
+  public GerenciamentoVotacao() {
+    this.pessoasCandidatas = new ArrayList<PessoaCandidata>();
+    this.pessoasEleitoras = new ArrayList<PessoaEleitora>();
+    this.cpfComputado = new ArrayList<String>();
+    this.totalVotos = 0;
+  }
+  
+  /**
+   * Método cadastrarPessoaCandidata.
+   *
+   */
   public void cadastrarPessoaCandidata(String nome, Integer numero) {
-    
+    for (PessoaCandidata pessoa : this.pessoasCandidatas) {
+      if (pessoa.getNumero().equals(numero)) {
+        System.out.println("Número pessoa candidata já utilizado!");
+        break;
+      }
+    }
+    PessoaCandidata novaPessoa = new PessoaCandidata(nome, numero);
+    this.pessoasCandidatas.add(novaPessoa);
+  }
+  
+  /**
+   * Método PessoaEleitora.
+   * 
+   */
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+    for (PessoaEleitora pessoa : this.pessoasEleitoras) {
+      if (pessoa.getCpf().equals(cpf)) {
+        System.out.println("Pessoa eleitora já utilizado!");
+        break;
+      }
+    }
+    PessoaEleitora novaPessoa = new PessoaEleitora(nome, cpf);
+    this.pessoasEleitoras.add(novaPessoa);
   }
   
 }
+
+
